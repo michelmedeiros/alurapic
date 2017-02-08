24b9ac709ead265c773608cf3fb538feddfeb7fc
Michel Medeiros <michel.tds@gmail.com>
2017-02-08 20:45:18 -0200

    Adicionando SPA

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,16 @@
-angular.module('alurapic', ['panelDiretiva', 'ngAnimate']);
+angular.module('alurapic', ['panelDiretiva', 'ngAnimate', 'ngRoute'])
+    .config(function($routeProvider, $locationProvider) {
+
+        $locationProvider.html5Mode(true);
+
+        $routeProvider.when('/fotos', {
+            templateUrl: 'partials/principal.html',
+            controller: 'FotosController'
+        });
+
+        $routeProvider.when('/fotos/new', {
+            templateUrl: 'partials/novo.html'
+        });
+
+        $routeProvider.otherwise({ redirectTo: '/fotos' });
+    });
