1c2045bab58f19a8fc6007aafdf7c82b32159aa9
Michel Medeiros <michel.tds@gmail.com>
2017-02-10 06:50:18 -0200

    Editando e removendo registros

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,19 +1,41 @@
 angular.module('alurapic')
-    .controller('FotoController', function($scope, $http) {
+    .controller('FotoController', function($scope, $http, $routeParams) {
         $scope.foto = {};
         $scope.mensagem = '';
+
+        if ($routeParams.fotoId) {
+            $http.get('v1/fotos/' + $routeParams.fotoId)
+                .success(function(foto) {
+                    $scope.foto = foto;
+                })
+                .error(function(erro) {
+                    $scope.mensagem = 'Não foi possível encontrar a foto';
+                });
+        }
+
+
         $scope.submeter = function() {
             if ($scope.formulario.$valid) {
-                console.log($scope.foto);
-                $http.post('v1/fotos', $scope.foto)
-                    .success(function() {
-                        $scope.mensagem = "Foto gravada com sucesso";
-                        $scope.foto = {};
-                    })
-                    .error(function(erro) {
-                        $scope.mensagem = "Erro na gravação dos dados." + erro;
-                    })
+                if ($scope.foto._id) {
+                    $http.put('v1/fotos/' + $scope.foto._id, $scope.foto)
+                        .success(function() {
+                            $scope.mensagem = "Foto atualizada com sucesso";
+                        })
+                        .error(function(erro) {
+                            $scope.mensagem = "Erro na atualização dos dados." + erro;
+                        })
+
+                } else {
+                    $http.post('v1/fotos', $scope.foto)
+                        .success(function() {
+                            $scope.mensagem = "Foto gravada com sucesso";
+                            $scope.foto = {};
+                        })
+                        .error(function(erro) {
+                            $scope.mensagem = "Erro na gravação dos dados." + erro;
+                        })
+
+                }
             }
         };
-
     });
--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,6 +1,8 @@
 angular.module('alurapic').controller('FotosController', function($scope, $http) {
     $scope.fotos = [];
     $scope.filtro = '';
+    $scope.mensagem = '';
+
     $http.get('v1/fotos')
         .success(function(data) {
             $scope.fotos = data;
@@ -8,4 +10,16 @@ angular.module('alurapic').controller('FotosController', function($scope, $http)
         .error(function(erro) {
             console.log('Erro encontrado na requisição Get: ' + erro);
         });
+
+    $scope.remover = function(foto) {
+        $http.delete('v1/fotos/' + foto._id)
+            .success(function() {
+                var indiceFoto = $scope.fotos.indexOf(foto);
+                $scope.fotos.splice(indiceFoto, 1);
+                $scope.mensagem = "Foto excluída com sucesso!";
+            })
+            .error(function(error) {
+                $scope.mensagem = "Erro ao remover foto!"
+            })
+    };
 });
